22be27998a81cc42997ea8e3e7583c6ac1dfde39
Dani Gutierrez <dgtzmonge@gmail.com>
2022-12-17 16:19:29 -0800

    hyprland

--- flake.nix
+++ flake.nix
@@ -3,13 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
     home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, home-manager, hyprland }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -23,6 +29,7 @@
           inherit system;
           modules = [
             ./configuration.nix
+
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
@@ -30,6 +37,9 @@
                 imports = [ ./home.nix ];
               };
             }
+
+            hyprland.nixosModules.default
+            { programs.hyprland.enable = true; }
           ];
         };
       };
