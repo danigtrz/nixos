e9d457d835a4cfaf4b47cf6cb9b643c6805c61da
Dani Gutierrez <dgtzmonge@gmail.com>
2022-12-29 13:33:41 -0800

    whitespace

--- modules/programs/neovim/neovim.nix
+++ modules/programs/neovim/neovim.nix
@@ -1,23 +1,22 @@
 { config, lib, pkgs, ...}:
 
 {
-programs.neovim = {
-enable = true;
-extraConfig = ''
-luafile /home/rafa/.nixos/modules/programs/neovim/settings.lua
-lua require'nvim-tree'.setup {}
-'';
-plugins = with pkgs.vimPlugins; [
-# file tree
-nvim-web-devicons
-nvim-tree-lua
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      luafile /home/rafa/.nixos/modules/programs/neovim/settings.lua
+      lua require'nvim-tree'.setup {}
+    '';
+    plugins = with pkgs.vimPlugins; [
+      # file tree
+      nvim-web-devicons
+      nvim-tree-lua
 
+      # languages
+      vim-nix
 
-# languages
-vim-nix
-
-indentLine
-];
-};
+      # other
+      indentLine
+    ];
+  };
 }
-
