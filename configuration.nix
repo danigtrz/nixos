91793e32b20e52c54b980ffa2d95fa4cd8157767
Dani Gutierrez <dgtzmonge@gmail.com>
2022-12-30 17:50:27 -0800

    failed attempt at life

--- configuration.nix
+++ configuration.nix
@@ -186,11 +186,14 @@ flake-overlays:
 	# fonts
 	fonts.fonts = with pkgs; [
 		noto-fonts
-		noto-fonts-cjk
+		noto-fonts-cjk-sans
+		noto-fonts-cjk-serif
 		noto-fonts-emoji
+		noto-fonts-extra
 		liberation_ttf
 		fira-code
 		fira-code-symbols
+		dejavu_fonts
 	];
 
 	# cachix for hyprland
