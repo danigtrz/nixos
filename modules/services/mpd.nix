8186e0cb0f7b86aec6a650508ef0f8ad94ff2469
Dani Gutierrez <dgtzmonge@gmail.com>
2023-01-10 21:21:18 -0800

    init

---
+++ modules/services/mpd.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+	services.mpd = {
+		enable = true;
+		musicDirectory = "${home.homeDirectory}/Music";
+		user = "rafa";
+		extraConfig = ''
+			audio_output {
+				type "pipewire"
+				name "PipeWire Sound Server"
+			}
+		'';
+	}
+}
