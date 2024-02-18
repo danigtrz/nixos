b0dd0d7117666b62db29c16d2f77f8e5305e24e7
Dani Gutierrez <dgtzmonge@gmail.com>
2024-02-17 18:19:06 -0800

    simplify config

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -8,19 +8,19 @@
 		};
 		histSize = 10000;
 		histFile = "~/.histfile";
-		enableCompletion = true;
-		enableAutosuggestions = true;
-		syntaxHighlighting.enable = true;
-		plugins = [
-			{
-				name = "zsh-autosuggestions";
-				src = pkgs.fetchFromGitHub {
-					owner = "zsh-users";
-					repo = "zsh-autosuggestions";
-					rev = "v0.7.0";
-					sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-				};
-			}
-		];
+		# enableCompletion = true;
+		# enableAutosuggestions = true;
+		# syntaxHighlighting.enable = true;
+		# plugins = [
+		# 	{
+		# 		name = "zsh-autosuggestions";
+		# 		src = pkgs.fetchFromGitHub {
+		# 			owner = "zsh-users";
+		# 			repo = "zsh-autosuggestions";
+		# 			rev = "v0.7.0";
+		# 			sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+		# 		};
+		# 	}
+		# ];
 	};
 }
