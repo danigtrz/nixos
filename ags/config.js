56a286a6d498fbbe4f822b5d86b1a90cbaed9ce8
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-03 16:21:42 -0800

    implemented

--- ags/config.js
+++ ags/config.js
@@ -3,10 +3,12 @@ import Gtk from 'gi://Gtk?version=3.0';
 Gtk.IconTheme.get_default().append_search_path('./assets');
 
 import bar from './js/bar/bar.js';
+import applauncher from './js/applauncher/applauncher.js';
 
 export default {
 	style: './style.css',
 	windows: [
-		bar()
+		bar(),
+		applauncher
 	]
 };
