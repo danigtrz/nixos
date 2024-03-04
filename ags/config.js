5e9de53548a9f09c2b47e4aee35cf3491bc52b08
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-03 16:27:17 -0800

    fix?

--- ags/config.js
+++ ags/config.js
@@ -5,10 +5,13 @@ Gtk.IconTheme.get_default().append_search_path('./assets');
 import bar from './js/bar/bar.js';
 import { applauncher } from './js/applauncher/applauncher.js';
 
+App.config({
+	windows: [applauncher]
+});
+
 export default {
 	style: './style.css',
 	windows: [
-		bar(),
-		applauncher
+		bar()
 	]
 };
