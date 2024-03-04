2b1a732ae259f94d1d99d39292d52c39ecd06b0d
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-03 16:26:07 -0800

    typo

--- ags/config.js
+++ ags/config.js
@@ -3,7 +3,7 @@ import Gtk from 'gi://Gtk?version=3.0';
 Gtk.IconTheme.get_default().append_search_path('./assets');
 
 import bar from './js/bar/bar.js';
-import applauncher from './js/applauncher/applauncher.js';
+import { applauncher } from './js/applauncher/applauncher.js';
 
 export default {
 	style: './style.css',
