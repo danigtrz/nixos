0c90f8fff5b6fd22e8e8c4c6608cd7a123bfa58f
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-09 20:03:56 -0800

    remove old configs

--- old/discord_patched.nix
+++
@@ -1,5 +0,0 @@
-with import <nixpkgs> {};
-
-pkgs.discord.override {
-    nss = pkgs.nss_3_89_1;
-}
--- old/modules/programs/ncmpcpp.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-	programs.ncmpcpp = {
-		enable = true;
-	};
-}
