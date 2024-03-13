d578018053f0fcbbd16e80b34fee31b5b8e16d8a
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-12 17:36:59 -0700

    move telescope maps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,2 @@
 vim.g.mapleader = " "
-
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
