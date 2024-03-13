d62dc0ebc590fd583a7982b6999508513c67af9c
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-12 17:37:13 -0700

    add maps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,4 +1,3 @@
-return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' }
-}
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
