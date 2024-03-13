42fa78484811773d360739692261c554802fb02d
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-12 17:10:54 -0700

    init

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smartindent = true
+vim.opt.expandtab = false
+
+vim.opt.number = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.breakindent = true
