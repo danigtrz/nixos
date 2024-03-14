eff67d634978d424cd487fd0cff4fa8bef6a3dfe
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-14 15:17:05 -0700

    fix catppuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,21 +1,15 @@
-return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			flavour = "mocha",
-			transparent_background = true,
-			show_end_of_buffer = false,
-			term_colors = false,
-			dim_inactive = {
-				enabled = false,
-			},
-			no_italic = false,
-			no_bold = false,
-			no_underline = false,
-			integrations = {
-				treesitter = true,
-			},
-		},
+require("catppuccin").setup({
+	flavour = "latte",
+	transparent_background = true,
+	show_end_of_buffer = false,
+	term_colors = false,
+	dim_inactive = {
+		enabled = false,
 	},
-}
+	no_italic = false,
+	no_bold = false,
+	no_underline = false,
+	integrations = {
+		treesitter = true,
+	},
+})
