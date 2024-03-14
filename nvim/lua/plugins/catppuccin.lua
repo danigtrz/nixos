320ad88548a79825d03df66ef84f32348ac44be1
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-13 19:57:20 -0700

    format

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,25 +1,21 @@
-require("catppuccin").setup({
-    flavour = "mocha",
-    transparent_background = true,
-    show_end_of_buffer = false,
-    term_colors = false,
-    dim_inactive = {
-        enabled = false,
-    },
-    no_italic = false,
-    no_bold = false,
-    no_underline = false,
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-    },
-})
-
-vim.cmd.colorscheme "catppuccin"
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			flavour = "mocha",
+			transparent_background = true,
+			show_end_of_buffer = false,
+			term_colors = false,
+			dim_inactive = {
+				enabled = false,
+			},
+			no_italic = false,
+			no_bold = false,
+			no_underline = false,
+			integrations = {
+				treesitter = true,
+			},
+		},
+	},
+}
