4ddfe4bab406843da576b2ba50fb9a067f8e204e
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-15 18:02:59 -0700

    format

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,15 +1,21 @@
-require("catppuccin").setup({
-	flavour = "latte",
-	transparent_background = false,
-	show_end_of_buffer = false,
-	term_colors = false,
-	dim_inactive = {
-		enabled = false,
-	},
-	no_italic = false,
-	no_bold = false,
-	no_underline = false,
-	integrations = {
-		treesitter = true,
-	},
-})
+return {
+	"catppuccin/nvim",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "latte",
+			transparent_background = false,
+			show_end_of_buffer = true,
+			term_colors = false,
+			dim_inactive = {
+				enabled = false,
+			},
+			no_italic = false,
+			no_bold = false,
+			no_underline = false,
+			integrations = {
+				treesitter = true,
+			},
+		})
+	end
+}
