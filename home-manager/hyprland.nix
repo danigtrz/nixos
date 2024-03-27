b30099459ee133d77d4e4350b5661406b370eab4
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-27 13:35:22 -0700

    ags launcher

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -122,8 +122,8 @@ in
 				"SUPER, J, togglesplit,"
 				"SUPER SHIFT, M, exit,"
 
-				"SUPER, R, exec, wofi --show drun"
-				# "SUPER, T, exec, ags -b hypr -t applauncher"
+				# "SUPER, R, exec, wofi --show drun"
+				"SUPER, R, exec, ags -t applauncher"
 
 				"SUPER, Return, exec, kitty"
 
@@ -171,9 +171,7 @@ in
 			bindle = [
 				", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
 				", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-				
-				# ", XF86KbdBrightnessDown, exec, light -U 5"
-				# ", XF86KbdBrightnessUp, exec, light -A 5"
+
 				", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
 				", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
 			];
