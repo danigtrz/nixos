c4acc280abc038ebf8bc459166de1864dfb48bb5
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-28 16:51:10 -0700

    notifications

--- ags/config.js
+++ ags/config.js
@@ -4,11 +4,23 @@
 
 import bar from './js/bar/bar.js';
 import { applauncher } from './js/applauncher/applauncher.js';
+import { NotificationPopups } from "./js/notifications/notification_popups.js";
+
+Utils.timeout(100, () => Utils.notify({
+    summary: "Notification Popup Example",
+    iconName: "info-symbolic",
+    body: "Lorem ipsum dolor sit amet, qui minim labore adipisicing "
+        + "minim sint cillum sint consectetur cupidatat.",
+    actions: {
+        "Cool": () => print("pressed Cool")
+    }
+}))
 
 export default {
 	style: './style.css',
 	windows: [
 		bar(),
+		NotificationPopups(),
 		applauncher
 	]
 };
