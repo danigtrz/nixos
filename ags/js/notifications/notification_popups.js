872fe6424556a85e740a50a7f94bd17f07400d51
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-30 17:42:54 -0700

    notifications

--- ags/js/notifications/notification_popups.js
+++ ags/js/notifications/notification_popups.js
@@ -33,65 +33,93 @@ function Notification(n) {
 
 	const title = Widget.Label({
 		class_name: "title",
-		xalign: 0,
-		justification: "left",
-		hexpand: true,
-		max_width_chars: 24,
-		truncate: "end",
-		wrap: true,
 		label: n.summary,
-		use_markup: true
+		max_width_chars: 24,
+		justification: "left",
+		truncate: "end",
+		halign: 1,
+		xalign: 0,
+		use_markup: true,
+		wrap: true
 	})
 
 	const body = Widget.Label({
 		class_name: "body",
-		hexpand: true,
-		use_markup: true,
-		xalign: 0,
-		justification: "left",
 		label: n.body,
+		justification: "left",
+		halign: 1,
+		xalign: 0,
+		hexpand: false,
+		vexpand: true,
+		use_markup: true,
 		wrap: true
 	})
 
 	const actions = Widget.Box({
 		class_name: "actions",
+		spacing: 8,
+		halign: 3,
 		children: n.actions.map(({ id, label }) => Widget.Button({
 			class_name: "action-button",
 			on_clicked: () => {
 				n.invoke(id)
 				n.dismiss()
 			},
-			hexpand: true,
 			child: Widget.Label(label)
 		}))
 	})
 
+	const text_box = Widget.Box({
+		class_name: "text-box",
+		vertical: true,
+		spacing: 4,
+		halign: 1,
+		valign: 3,
+		children: [ title ]
+	})
+
+	// only add body if there is text
+	if (body.label.trim() !== "") {
+		text_box.add(body);
+	}
+
+	const info_box = Widget.Box({
+		class_name: "info-box",
+		halign: 1,
+		children: [
+			icon,
+			text_box
+		]
+	})
+
+	const notification_box = Widget.Box({
+		class_name: `notification ${n.urgency}`,
+		vertical: true,
+		spacing: 16,
+		halign: 3,
+		valign: 3,
+		children: [ info_box ]
+	})
+
+	if (n.actions.length > 0) {
+		notification_box.add(actions);
+	}
+
 	return Widget.EventBox(
 		{
 			attribute: { id: n.id },
-			on_primary_click: n.dismiss
+			on_primary_click: n.dismiss,
+			halign: 3,
 		},
-		Widget.Box(
-			{
-				class_name: `notification ${n.urgency}`,
-				vertical: true
-			},
-			Widget.Box([
-				icon,
-				Widget.Box(
-					{ vertical: true },
-					title,
-					body
-				)
-			]),
-			actions
-		)
+		notification_box
 	)
 }
 
 export function NotificationPopups(monitor = 0) {
 	const list = Widget.Box({
+		class_name: "notifications",
 		vertical: true,
+		spacing: 16,
 		children: notifications.popups.map(Notification),
 	})
 
@@ -112,19 +140,7 @@ export function NotificationPopups(monitor = 0) {
 		monitor,
 		name: `notifications-${monitor}`,
 		class_name: "notification-popups",
-		anchor: ["top", "right"],
-		child: Widget.Box({
-			css: "min-width: 2px; min-height: 2px;",
-			class_name: "notifications",
-			vertical: true,
-			child: list,
-
-			/** this is a simple one liner that could be used instead of
-				hooking into the 'notified' and 'dismissed' signals.
-				but its not very optimized becuase it will recreate
-				the whole list everytime a notification is added or dismissed */
-			// children: notifications.bind('popups')
-			//     .as(popups => popups.map(Notification))
-		}),
+		anchor: ["top"],
+		child: list,
 	})
 }
