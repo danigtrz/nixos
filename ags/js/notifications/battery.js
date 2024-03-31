4f14ab01fc2b8e8500d4f9f9276c8b6c654bd43a
Dani Gutierrez <dgtzmonge@gmail.com>
2024-03-30 17:52:31 -0700

    update battery notification message

--- ags/js/notifications/battery.js
+++ ags/js/notifications/battery.js
@@ -1,12 +1,12 @@
 export default async function init() {
 	const bat = await Service.import("battery")
 	bat.connect("notify::percent", ({ percent, charging }) => {
-		if (percent !== 68 || charging)
+		if (percent !== 15 || charging)
 			return
 
 		Utils.notify({
 			summary: "Low Battery",
-			body: `Battery at ${percent}%. Consider not running out of battery.`,
+			body: `Battery is at ${percent}%. Consider doing something about it.`,
 			iconName: "battery-low-symbolic",
 			urgency: "critical"
 		})
