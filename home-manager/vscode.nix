3e6fa852e9aff95a5da9e93c6182ab5282d6ef4f
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2024-04-20 18:27:17 -0700

    not fhs

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -3,32 +3,31 @@
 {
 	programs.vscode = {
 		enable = true;
-		package = pkgs.vscode.fhs;
-		# extensions = with pkgs.vscode-extensions; [
-		# 	# nix
-		# 	bbenoist.nix
+		extensions = with pkgs.vscode-extensions; [
+		 	# nix
+		 	bbenoist.nix
 
-		# 	# python
-		# 	ms-python.python
-		# 	ms-python.vscode-pylance
-		# 	ms-tollsai.jupyter
+		 	# python
+		 	ms-python.python
+		 	ms-python.vscode-pylance
+		 	ms-toolsai.jupyter
 
-		# 	# c
-		# 	ms-vscode.cpptools-extension-pack
+		 	# c
+		 	ms-vscode.cpptools-extension-pack
 
-		# 	# latex
-		# 	james-yu.latex-workshop
+		 	# latex
+		 	james-yu.latex-workshop
 
-		# 	# ai
-		# 	github.copilot
-		# 	github.copilot-chat
+		 	# ai
+		 	github.copilot
+		 	github.copilot-chat
 
-		# 	# vim
-		# 	vscodevim.vim
+		 	# vim
+		 	vscodevim.vim
 
-		# 	# theme
-		# 	catppuccin.catppuccin-vsc
-		# 	catppuccin.catppuccin-vsc-icons
-		# ];
+		 	# theme
+		 	catppuccin.catppuccin-vsc
+		 	catppuccin.catppuccin-vsc-icons
+		];
 	};
 }
