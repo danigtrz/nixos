07a07797b135aee9ee5f727a05d44f2af22ae4bc
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2024-06-05 14:20:55 -0700

    nvidia driver version

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -16,7 +16,7 @@
 			nvidiaBusId = "PCI:1:0:0";
 		};
 
-		package = config.boot.kernelPackages.nvidiaPackages.latest;
+		package = config.boot.kernelPackages.nvidiaPackages.beta;
 	};
 
 	hardware.opengl = {
