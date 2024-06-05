956e5ce4cfa9e03eca1a84866a29b9d02b975421
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2024-06-05 14:05:43 -0700

    wlr hw cursor env var

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -22,7 +22,6 @@ in
 		XCURSOR_SIZE = "24";
 
 		XDG_SESSION_TYPE = "wayland";
-		WLR_NO_HARDWARE_CURSORS = "1";
 		LIBVA_DRIVER_NAME = "nvidia";
 		# GBM_BACKEND = "nvidia-drm";
 		# __GLX_VENDOR_LIBRARY_NAME = "nvidia";
