ba237d6f1594845df68e715c162a7f1c15b387b2
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-01-19 20:47:04 -0800

    xwayland

--- home-manager/ags.nix
+++ home-manager/ags.nix
@@ -12,6 +12,4 @@
 			accountsservice
 		];
 	};
-
-	home.packages = [inputs.astal.packages.${system}.default];
 }
--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -32,6 +32,10 @@
 				disable_hyprland_logo = true;
 			};
 
+			xwayland = {
+				force_zero_scaling = true;
+			};
+
 			input = {
 				kb_layout = "us";
 				kb_variant = "altgr-intl";
