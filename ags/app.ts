f8cc87f6c1bd7b8e4c0945f286eec3ac25d0269c
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-01-23 22:49:03 -0800

    ags

--- ags/app.ts
+++ ags/app.ts
@@ -2,6 +2,7 @@ import { App } from "astal/gtk3"
 import Bar from "./widget/bar"
 
 App.start({
+	request
 	main() {
 		Bar()
 	}
--- ags/widget/bar.tsx
+++
@@ -1,5 +0,0 @@
-function Bar(monitor = 0) {
-	return <window className="Bar" monitor={monitor}>
-		<box>content of bar</box>
-	</window>
-}
