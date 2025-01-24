149079abf7ab9898b2443ff507f0eb44aa3f5e84
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-01-23 23:16:41 -0800

    ags

--- ags/app.ts
+++ ags/app.ts
@@ -1,8 +1,10 @@
 import { App } from "astal/gtk3"
 import Bar from "./windows/bar/bar"
+import Applauncher from "./windows/applauncher/applauncher"
 
 App.start({
 	main() {
 		Bar()
+		Applauncher()
 	}
 })
