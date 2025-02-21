4437ff8272a723cebe1e6b0422c0c2f0836ede2e
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-02-20 20:04:45 -0800

    gnome

---
+++ nixos/gnome.nix
@@ -0,0 +1,36 @@
+{ inputs, pkgs, config, ... }:
+
+{
+	services.xserver = {
+		enable = true;
+		displayManager.gdm.enable = true;
+		displayManager.gnome.enable = true;
+	}
+
+	environment.gnome.excludePackages = (with pkgs; [
+		atomix
+		cheese
+		epiphany
+		evince
+		geary
+		gedit
+		gnome-characters
+		gnome-music
+		gnome-photos
+		gnome-terminal
+		gnome-tour
+		hitori
+		iagno
+		tali
+		totem
+	]);
+
+	programs.dconf.enable = true;
+
+	environment.systemPackages = [
+		gnome.adwaita-icon-theme
+		pkgs.gnomeExtensions.appindicator
+	];
+
+	services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+}
