0665ea0c1d697ccdc885c1ce6557c52d15990c69
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-04-28 14:50:14 -0700

    brightness keys

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -131,8 +131,8 @@
 				", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
 				", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
 
-				# ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
-				# ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
+				", XF86MonBrightnessDown, exec, brightnessctl -d intel_backlight set 5%-"
+				", XF86MonBrightnessUp, exec, brightnessctl -d intel_backlight set +5%"
 			];
 
 			bindm = [
