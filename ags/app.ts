d5d399637494993c8c4af2b222167b59b586a10e
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-04-30 12:33:14 -0700

    init bar

--- ags/app.ts
+++ ags/app.ts
@@ -1,10 +1,13 @@
 import { App } from "astal/gtk3"
 import Bar from "./windows/bar/bar"
 import Applauncher from "./windows/applauncher/applauncher"
+import style from "./style.scss"
 
 App.start({
+	css: style,
+	instanceName: "interface",
 	main() {
 		Bar()
-		Applauncher()
+		// Applauncher()
 	}
 })
