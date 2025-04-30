98a8a81cebace24ffc8e45ee66681adefaa49092
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-04-29 18:54:48 -0700

    fixed vscode option

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -3,7 +3,7 @@
 {
 	programs.vscode = {
 		enable = true;
-		extensions = with pkgs.vscode-extensions; [
+		profiles.default.extensions = with pkgs.vscode-extensions; [
 		 	# nix
 		 	bbenoist.nix
 
