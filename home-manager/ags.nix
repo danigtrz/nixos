43b745c85d92c3c6902099f6bf0e1feb28f7b77a
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-04-30 12:38:30 -0700

    bundling

--- home-manager/ags.nix
+++ home-manager/ags.nix
@@ -13,7 +13,23 @@
 	};
 
 	home.packages = [
-		inputs.ags.packages.${pkgs.system}.io
-		inputs.ags.packages.${pkgs.system}.notifd
+		(inputs.ags.lib.bundle {
+			inherit pkgs;
+			src = ../ags;
+			name = "desktop-shell"; # executable name
+			gtk4 = true;
+			entry = "app.ts";
+			
+			extraPackages = with inputs.ags.packages.${pkgs.system}; [
+				apps # app launcher
+				mpris # media controls
+				hyprland # workspace integration
+				bluetooth # bluez integration
+				battery 
+				wireplumber # used by pipewire
+				notifd # desktop notification integration
+			];
+	  	})
+		inputs.ags.packages.${pkgs.system}.io # astal cli
 	];
 }
