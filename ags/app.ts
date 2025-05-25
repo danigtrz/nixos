80ce6444b5c99a7b310b4b774e66ed238f656205
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-05-24 19:30:53 -0700

    added notifications and osd

--- ags/app.ts
+++ ags/app.ts
@@ -1,17 +1,17 @@
 import { App } from "astal/gtk3"
 import Bar from "./windows/bar/bar"
 import Applauncher from "./windows/applauncher/applauncher"
-// import NotificationPopups from "./notifications/NotificationPopups"
-import OSD from "./osd/OSD"
+import NotificationPopups from "./windows/notifications/NotificationPopups"
+import OSD from "./windows/osd/OSD"
 import style from "./style.scss"
 
 App.start({
 	css: style,
 	instanceName: "interface",
 	main() {
-		Bar()
+		App.get_monitors().map(Bar)
 		Applauncher()
-		// App.get_monitors().map(NotificationPopups)
+		App.get_monitors().map(NotificationPopups)
 		App.get_monitors().map(OSD)
 	}
 })
