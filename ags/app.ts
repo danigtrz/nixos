1cbd6a9bfa2914dba868c0d5a8da02297c2287e7
Daniel Gutierrez Monge <dgtzmonge@gmail.com>
2025-05-24 19:15:04 -0700

    trying

--- ags/app.ts
+++ ags/app.ts
@@ -1,6 +1,7 @@
 import { App } from "astal/gtk3"
 import Bar from "./windows/bar/bar"
 import Applauncher from "./windows/applauncher/applauncher"
+import NotificationPopups from "./notifications/NotificationPopups"
 import style from "./style.scss"
 
 App.start({
@@ -9,5 +10,6 @@ App.start({
 	main() {
 		Bar()
 		Applauncher()
+		NotificationPopups()
 	}
 })
